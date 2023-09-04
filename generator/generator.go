b5a31c29b6585ca15a5951a39ac00657b70666fd
Nikkeh <nikkehtine@int.pl>
2023-09-04 23:16:14 +0200

    generator: add character groups

--- generator/generator.go
+++ generator/generator.go
@@ -1,13 +1,20 @@
 package generator
 
+const (
+	lowercase_chars = "abcdefghijklmnopqrstuvwxyz"
+	uppercase_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	numbers         = "0123456789"
+	symbols         = "!@#$%^&*-_=+"
+	similar         = "iIl1oO0vw"
+)
+
 type GeneratorConfig struct {
-	length            int
-	lowercase_chars   bool
-	uppercase_chars   bool
-	numbers           bool
-	symbols           bool
-	exclude_similar   bool
-	exclude_ambiguous bool
+	length          int
+	lowercase_chars bool
+	uppercase_chars bool
+	numbers         bool
+	symbols         bool
+	exclude_similar bool
 }
 
 // Generates a password based on provided config
